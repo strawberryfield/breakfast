3fe902019dd6c97631057b102f3eb1a7c81ea7b1
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2024-08-10 14:42:29 +0200

    released with interpreter

--- package.cmd
+++ package.cmd
@@ -14,23 +14,31 @@ set PROJECT_HOME=breakfast
 set NAME=Breakfast in the Dolomites
 set RAR="C:\Program Files\WinRAR\WinRAR.exe"
 set COMMENT=zipcomment.txt
-set PROJECT_PATH=%DOC_ROOT%%DOC_PATH%\Projects\%PROJECT_HOME%\%NAME%.inform
+set HOME_PATH=%DOC_ROOT%%DOC_PATH%\Projects\%PROJECT_HOME%
+set PROJECT_PATH=%HOME_PATH%\%NAME%.inform
+set MATERIALS_PATH=%HOME_PATH%\%NAME%.materials
 set BUILD_DIR=%PROJECT_PATH%\Build\
-set RELEASE_DIR=%DOC_ROOT%%DOC_PATH%\Projects\%PROJECT_HOME%\%NAME%.materials\Release\
+set RELEASE_DIR=%MATERIALS_PATH%\Release\
+set INTERPRETER_DIR=%RELEASE_DIR%interpreter\
 set BLURB="%PROJECT_PATH%\Release.blurb"
+set BIN=%HOME_PATH%\bin\%NAME%
 set COPYRIGHT="copyright (c) 2024 Roberto Ceccarelli - The Strawberry Field"
 
+rem delete old files 
+del "%BIN%.zip"
+del /s /q "%MATERIALS_PATH%\release\*.*"
+
 rem project home
-%DOC_ROOT%
-cd "%DOC_PATH%\%PROJECT_HOME%"
+pushd "%DOC_ROOT%%DOC_PATH%\%PROJECT_HOME%"
 
 rem build project
 %Inform7% -release -internal %INTERNAL% -external %EXTERNAL% -project "%PROJECT_PATH%" -format=Inform6/32d
 %Inform6% -E2w~S~DG "%BUILD_DIR%auto.inf" "%BUILD_DIR%output.ulx"
 echo copyright %COPYRIGHT% >> %BLURB%
 %InBlorb% %BLURB% "%RELEASE_DIR%%NAME%.gblorb"
+copy "%MATERIALS_PATH%\resourcemap.js" "%INTERPRETER_DIR%"
 
 rem packaging
-cd "%NAME%.materials"
-%RAR% a -afzip -ep -m5 -z%COMMENT% "..\bin\%NAME%" release
-cd ..
+cd "%MATERIALS_PATH%\release"
+%RAR% a -afzip -r -m5 -z..\%COMMENT% "%BIN%" *.*
+popd
